d262c78033c9f01cb7b11c81a174ec14391b54ed
Nihat M. Khidirov <nihatmkh@code.local>
2023-02-04 13:00:48 +0400

    init

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededinizi daxil edin");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            if (e < 100)
+                Console.WriteLine("Duzgun daxil edilmemisdir");
+
+            else if (e <= 999.999)
+            {
+                double f = e * 1000;
+                double g = f % 1000;
+                int h = (int)(f / 1000);
+
+                Console.WriteLine(g + (h * 0.001));
+            }
+
+            else 
+                Console.WriteLine("Bu say duzgun deyil");
+
+
+
+
+        }
+    }
+}
